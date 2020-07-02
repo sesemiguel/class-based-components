cec61ee6e76fbe80d72a844c86e3d13cd7a98f5b
Sese <sese.miguel9@gmail.com>
2020-07-02 21:02:11 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    // This will return everything we have in this 
+    // functional component
+
+    /*
+    * You can also implement a method here
+    */
+
+    // When using props on the return method:
+    // {this.props.whatever}
+
+    render() {
+        /*
+        * Any kind of display logic, do it here before the return
+        * Eg. Conditional rendering
+        */
+
+        /*
+        * const style = this.yourMethodHere()
+        */
+
+        return (
+            <>
+                <h1>Hello World</h1>
+                {/* {this.props.whatever} */}
+            </>
+        )
+    }
 }
 
-export default App;
+export default App
